3b9df3d688ca6545f07c29768b709c727babb6cc
allen2001 <806952601@qq.com>
2019-01-07 13:55:02 +0800

    update

--- src/Upload.js
+++ src/Upload.js
@@ -24,16 +24,25 @@ Upload.prototype.init = function() {
 	var _this = this;
 	// 绑定点击事件 选择文件
 	this.btn.addEventListener('click', function(ev) {
-		// 创建一个file input (只能选择一个文件)
+		// 创建一个file input (默认只能选择一个文件)
 		var input = document.createElement('input');
-		input.type = 'file';
+    input.type = 'file';
+    input.style.display = 'none';
+    // 判断是否能选择多个文件上传
+    if (_this.options.multiple) {
+      input.setAttribute('multiple', 'multiple');
+    }
 		// 绑定input 选择文件后的回调
 		input.onchange = function() {
-			// 获取文件对象
-			var file = this.files[0];
+			// 获取已选择的文件对象集合
+      var files = this.files;
+      
 			// 创建formdata
-			var formdata = new FormData();
-			formdata.append(_this.options.name, file);
+      var formdata = new FormData();
+      for (var i = 0; i < files.length; i++) {
+        formdata.append(_this.options.name, files[i]);
+      }
+      
 			// 发起一个ajax请求
 			var xhr = new XMLHttpRequest();
 
@@ -48,37 +57,40 @@ Upload.prototype.init = function() {
 				}
 			})
 			// 监听上传结束的事件(不管成功与失败)
-			xhr.addEventListener('loadend', _this.options.onEnd)
+      xhr.addEventListener('loadend', _this.options.onEnd);
+      // 监听onload事件
+      xhr.addEventListener('load', _this.options.onLoad);
+      // 监听onerror事件
+      xhr.addEventListener('error', _this.options.onError);
 
 			var url = _this.options.url;
 			xhr.open('POST', url);
 			// 监听事件
 			xhr.onreadystatechange = function() {
 				if (xhr.readyState == 4) {
-					var res = xhr.responseText;
 					if (xhr.status == 200) {
 						// 请求成功
 						_this.options.onSuccess({
 							status: 200,
-							data: res
+							data: xhr.responseText
 						});
 					} else {
 						// 请求失败
 						_this.options.onError({
 							code: xhr.status,
-							message: res
+							message: xhr.statusText
 						});
 					}
-				} else {
-					// 请求未完成
-
 				}
 			}
 			// 发送请求
 			xhr.send(formdata);
 		}
-		// 自动触发选择文件操作
-		input.click();
+    // 自动触发选择文件操作
+    document.body.appendChild(input);
+    input.click();
+    document.body.removeChild(input);
+    
 	}, false);
 }
 
